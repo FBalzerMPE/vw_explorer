2afc868a318b0b02d4967161b75588d1b1c99354
FBalzerMPE <fbalzer@mpe.mpg.de>
2025-11-19 05:11:24 +0100

    Trying support for python versions without up-to-date setuptools

--- setup.py
+++ setup.py
@@ -1,3 +1,40 @@
+"""Fallback script to setup the package using setuptools by reading metadata from pyproject.toml."""
+
+from pathlib import Path
+
+import toml
 from setuptools import setup
 
-setup()
+
+def load_pyproject_metadata():
+    pyproject_path = Path(__file__).parent / "pyproject.toml"
+    with pyproject_path.open("r") as f:
+        pyproject_data = toml.load(f)
+    project = pyproject_data.get("project", {})
+    return {
+        "name": project.get("name", "UNKNOWN"),
+        "version": project.get("version", "0.0.0"),
+        "description": project.get("description", ""),
+        "author": ", ".join(a.get("name", "") for a in project.get("authors", [])),
+        "author_email": ", ".join(
+            a.get("email", "") for a in project.get("authors", [])
+        ),
+        "license": project.get("license", {}).get("file", ""),
+        "keywords": project.get("keywords", []),
+        "classifiers": project.get("classifiers", []),
+        "install_requires": project.get("dependencies", []),
+        "entry_points": {
+            "console_scripts": [
+                f"{k}={v}" for k, v in project.get("scripts", {}).items()
+            ]
+        },
+    }
+
+
+# Load metadata and pass it to setup()
+metadata = load_pyproject_metadata()
+
+setup(
+    packages=["vw_explorer"],
+    **metadata,
+)
